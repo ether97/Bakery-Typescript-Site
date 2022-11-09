25b79c345ae5157aa92bcb5aeadab3a2a6cd64f4
Alex <orion6174@protonmail.com>
2022-11-08 20:21:14 -0500

    first commit

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -5,6 +5,10 @@ type ShoppingCartProviderProps = {
 };
 
 type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
   getItemQuantity: (id: number) => number;
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
@@ -24,6 +28,11 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
 
   function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
@@ -74,6 +83,8 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
         increaseCartQuantity,
         decreaseCartQuantity,
         removeFromCart,
+        cartItems,
+        cartQuantity,
       }}
     >
       {children}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,15 @@
 import type * as CSS from "csstype";
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  DropdownButton,
+  Dropdown,
+} from "react-bootstrap";
 import storeItems from "../data/items.json";
 import { StoreItem } from "../Components/StoreItem";
+import { useEffect, useState } from "react";
 
 const imgWrapper: CSS.Properties = {
   backgroundImage: "url('sera-iZgQKxuMRHc-unsplash.jpg')",
@@ -11,10 +19,14 @@ const imgWrapper: CSS.Properties = {
   height: "75vh",
   marginBottom: "100px",
   position: "relative",
+  width: "100%",
 };
 
 const container: CSS.Properties = {
   height: "200vh",
+  display: "flex",
+  flexDirection: "column",
+  // alignItems: "center",
 };
 
 const imgWrapperCover: CSS.Properties = {
@@ -36,22 +48,153 @@ const cakes: CSS.Properties = {
   textAlign: "center",
 };
 
+const storeBtn: CSS.Properties = {
+  height: "50px",
+  width: "200px",
+  marginBottom: "10px",
+};
+
 export function Store() {
+  const [newStoreItems, setNewStoreItems] = useState(storeItems);
+  const [title, setTitle] = useState("Bakery");
+  const [flavor, setFlavor] = useState("Filter Flavors");
+
+  function handleBakery(input: string): void {
+    if (input === "Cakes") {
+      setTitle(input);
+      setNewStoreItems(storeItems.filter((item) => item.id <= 9));
+    } else if (input === "Cookies") {
+      setTitle(input);
+      setNewStoreItems(
+        storeItems.filter((item) => item.id <= 18 && item.id > 9)
+      );
+    } else if (input === "Pies") {
+      setTitle(input);
+      setNewStoreItems(
+        storeItems.filter((item) => item.id <= 24 && item.id > 18)
+      );
+    } else {
+      setTitle("Bakery");
+      setNewStoreItems(storeItems);
+    }
+    return setFlavor("Filter Flavors");
+  }
+
+  function handleFlavor(input: string): void {
+    if (input === "Chocolate") {
+      if (title !== "Bakery") {
+        setFlavor(input);
+        setNewStoreItems(
+          newStoreItems.filter((item) => item.name.substring(0, 2) === "Ch")
+        );
+      } else {
+        setFlavor(input);
+        setNewStoreItems(
+          storeItems.filter((item) => item.name.substring(0, 2) === "Ch")
+        );
+      }
+    } else if (input === "Vanilla") {
+      if (title !== "Bakery") {
+        setFlavor(input);
+        setNewStoreItems(
+          newStoreItems.filter((item) => item.name.substring(0, 2) === "Va")
+        );
+      } else {
+        setFlavor(input);
+        setNewStoreItems(
+          storeItems.filter((item) => item.name.substring(0, 2) === "Va")
+        );
+      }
+    } else if (input === "Carrot") {
+      if (title !== "Bakery") {
+        setFlavor(input);
+        setNewStoreItems(
+          newStoreItems.filter((item) => item.name.substring(0, 2) === "Ca")
+        );
+      } else {
+        setFlavor(input);
+        setNewStoreItems(
+          storeItems.filter((item) => item.name.substring(0, 2) === "Ca")
+        );
+      }
+    }
+  }
   return (
     <div style={container}>
       <div style={imgWrapper}>
-        <h1 style={cakes}>Cakes</h1>
+        <h1 style={cakes}>{title}</h1>
         <div style={imgWrapperCover}></div>
       </div>
 
-      <Container className="mb-5">
-        <Row md={2} xs={1} lg={3} className="g-3">
-          {storeItems.map((item) => (
-            <Col>
-              <StoreItem key={item.id} {...item} />
-            </Col>
-          ))}
-        </Row>
+      <Container className="mb-5 d-flex g-10 flex-column">
+        <div className="d-flex align-items-center w-100">
+          <DropdownButton
+            align="end"
+            title={flavor}
+            id="dropdown-menu-align-end"
+            variant="dark"
+            className="float-right"
+            style={{
+              marginLeft: "auto",
+              marginBottom: "10px",
+            }}
+          >
+            <Dropdown.Item eventKey="1" onClick={() => handleFlavor("Vanilla")}>
+              Vanilla
+            </Dropdown.Item>
+            <Dropdown.Item
+              eventKey="2"
+              onClick={() => handleFlavor("Chocolate")}
+            >
+              Chocolate
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="3" onClick={() => handleFlavor("Carrot")}>
+              Carrot
+            </Dropdown.Item>
+          </DropdownButton>
+        </div>
+
+        <div className="d-flex justify-content-between">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Button
+              variant="outline-dark"
+              style={storeBtn}
+              onClick={() => handleBakery("All")}
+            >
+              All
+            </Button>
+            <Button
+              variant="outline-dark"
+              style={storeBtn}
+              onClick={() => handleBakery("Cakes")}
+            >
+              Cakes
+            </Button>
+            <Button
+              variant="outline-dark"
+              style={storeBtn}
+              onClick={() => handleBakery("Cookies")}
+            >
+              Cookies
+            </Button>
+            <Button
+              variant="outline-dark"
+              style={storeBtn}
+              onClick={() => handleBakery("Pies")}
+            >
+              Pies
+            </Button>
+          </div>
+          <div style={{ width: "80%" }}>
+            <Row md={2} xs={1} lg={3} className="g-3">
+              {newStoreItems.map((item) => (
+                <Col>
+                  <StoreItem key={item.id} {...item} />
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </div>
       </Container>
     </div>
   );
